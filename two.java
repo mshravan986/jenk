e7a54f909d04d71e7c4100e6ea2a889abb61474b
mshravan986 <mshravan986@gmail.com>
2018-04-28 13:05:40 +0530

    number pattern

--- two.java
+++ two.java
@@ -1,10 +1,14 @@
-public class Use_For_Loop
+class two
 {
-    public static void main(String[] args)
-    {
-        for(int i = 1; i <= 10; i++)
-        {
-            System.out.println(i);
-        }
-    }
+ public static void main(String[] args) 
+ {
+ for (int i = 1; i <= 5; i++) 
+ {
+ for (int j = 1; j <= i; j++)
+ {
+ System.out.print(j+" ");
+ }
+ System.out.println();
+ }
+ }
 }
